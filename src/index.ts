3dd9e9dbcce825fbbf3f95ff9e10f626c6da4036
mirayatech <mirayaabrodi@gmail.com>
2025-07-13 21:16:21 +0200

    feat: create index.ts to export animation components for streamlined imports

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { PageTransition, PageTransitionLegacy } from "./PageTransition";
+import { RevealOnScroll } from "./RevealOnScroll";
+import { AppRouterTransition } from "./AppRouterTransition";
+
+export {
+  PageTransition,
+  PageTransitionLegacy,
+  RevealOnScroll,
+  AppRouterTransition,
+};
